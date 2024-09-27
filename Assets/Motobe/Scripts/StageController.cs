c24cbcefe51937c5dddb9962df00c62b7e6f7a5c
Yuki <sdt22csc0018@edu.sca.ac.jp>
2024-09-27 17:56:00 +0900

    ステージ

---
+++ Assets/Motobe/Scripts/StageController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageController : MonoBehaviour
+{
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += new Vector3(-speed*Time.deltaTime,0,0);
+    }
+}
