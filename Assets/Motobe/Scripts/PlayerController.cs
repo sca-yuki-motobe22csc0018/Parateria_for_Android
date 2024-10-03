e729c199170ab706eb417878f825cfc262ed4609
Yuki <sdt22csc0018@edu.sca.ac.jp>
2024-10-03 16:16:19 +0900

    ステージを自動生成する

--- Assets/Motobe/Scripts/PlayerController.cs
+++ Assets/Motobe/Scripts/PlayerController.cs
@@ -7,21 +7,58 @@ public class PlayerController : MonoBehaviour
     //Rigidbody
     private Rigidbody2D rb;
 
+    //位置関係
+    public float DefaultPosition;
+    public float PlayerSpeed;
+
     //ジャンプ関連
     public float JumpForce;
+    public int MaxJumpCount;
+    private int thisJumpCount;
+    private bool onWall;
 
     // Start is called before the first frame update
     void Start()
     {
+        onWall = false;
         rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && thisJumpCount < MaxJumpCount)
         {
             rb.velocity = new Vector3(0, JumpForce, 0);
+            thisJumpCount++;
+        }
+        if (this.transform.position.x < DefaultPosition)
+        {
+            this.transform.position += new Vector3(PlayerSpeed * Time.deltaTime, 0, 0);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Stage")&&!onWall)
+        {
+            thisJumpCount = 0;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Wall"))
+        {
+            onWall = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Wall"))
+        {
+            onWall = false;
         }
     }
 }
--- Assets/Motobe/Scripts/StageController.cs
+++ Assets/Motobe/Scripts/StageController.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class StageController : MonoBehaviour
 {
     public float speed;
+    public float spawnPointX;
+    public float spawnPointY;
+    public float spawnPointZ;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,10 +21,18 @@ public class StageController : MonoBehaviour
         transform.position += new Vector3(-speed*Time.deltaTime,0,0);
     }
 
+    private void Stage00(float x, float y,float z)
+    {
+        GameObject Stage_prefab = Resources.Load<GameObject>("Stage00");
+        GameObject Stage = Instantiate(Stage_prefab, new Vector3(x, y, z), Quaternion.identity);
+        return;
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("DestroyObject"))
         {
+            Stage00(spawnPointX, spawnPointY,spawnPointZ);
             Destroy(this.gameObject);
         }
     }
